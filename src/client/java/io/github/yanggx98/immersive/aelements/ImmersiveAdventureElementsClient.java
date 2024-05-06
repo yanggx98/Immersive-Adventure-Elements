f354e235c24a6ee8013a7daca9617c69a39113f3
yanggx98 <yanggx98@outlook.com>
2024-05-07 02:34:31 +0800

    first commit

---
+++ src/client/java/io/github/yanggx98/immersive/aelements/ImmersiveAdventureElementsClient.java
@@ -0,0 +1,10 @@
+package io.github.yanggx98.immersive.aelements;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImmersiveAdventureElementsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
